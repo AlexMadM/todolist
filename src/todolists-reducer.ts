6b3433198a31c70ae40ee8e151f0b8aab25495e3
Alex <alex.alexandrov.1988@gmail.com>
2023-12-26 21:16:54 +0300

    lesson-7

--- src/todolists-reducer.ts
+++ src/todolists-reducer.ts
@@ -1,4 +1,4 @@
-import {TodolistType} from "./App";
+import {FilterValuesType, TodolistType} from "./App";
 import {v1} from "uuid";
 
 export const todolistsReducer=(state:TodolistType[],action:TodolistACType):TodolistType[]=>{
@@ -15,16 +15,19 @@ switch (action.type) {
         let newTodolist:TodolistType={id:newTodolistId,title:action.title,filter:'all'}
         return [newTodolist, ...state]
     }
+    case 'CHANGE-FILTER':{
+        return state.map(el=> el.id=== action.id ? {...el,filter:action.filter} :el)
+    }
         default:return state
 }
 }
 
-type TodolistACType =RemoveTodolistACType | ChangeTodolistTitleACType | AddTodolistACType
+type TodolistACType =RemoveTodolistACType | ChangeTodolistTitleACType | AddTodolistACType | ChangeFilterACType
 
 type RemoveTodolistACType = ReturnType<typeof removeTodolistAC>
 type ChangeTodolistTitleACType = ReturnType<typeof changeTodolistTitleAC>
 type AddTodolistACType =ReturnType<typeof addTodolistAC>
-
+type ChangeFilterACType=ReturnType<typeof changeFilterAC>
 
 export const removeTodolistAC=(id:string)=>{
     return{
@@ -41,5 +44,11 @@ export const changeTodolistTitleAC=(id:string,title:string)=>{
 export const addTodolistAC=(title:string)=>{
     return {
         type:'ADD-TODOLIST', title
+    }as const
+}
+
+export const changeFilterAC= (filter:FilterValuesType,id:string)=>{
+    return {
+        type: 'CHANGE-FILTER',filter,id
     }as const
 }
